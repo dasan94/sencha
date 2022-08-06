16aa031f8875cbb5282481883a026779cc37e758
Santiago Arizabaleta <caribaroja@hotmail.com>
2022-08-06 16:57:18 -0500

    feat:cambios sencha

--- features/pageobjects/home.page.ts
+++ features/pageobjects/home.page.ts
@@ -29,19 +29,82 @@ class HomePage extends Page {
       console.log("#### el ####");
       console.log(textvalue)
     });  
-    await $('//*[@id="thumbnails-1040"]/div[1]').click();
-    await browser.pause(5000);
 
+    //Engreso home click boton Buttos
     await $('//*[@id="thumbnails-1040"]/div[1]').click();
-    await browser.pause(5000);
+    await browser.pause(1000);
+    //#########################  Basic Buttons 
+//despues de dar click en Buttos redirige a typos de botones click Basick buttons
+   await $('//*[@id="thumbnails-1040"]/div[1]').click();
+        await browser.pause(1000);
 
+  // redirige a options
     await $('//*[@id="thumbnails-1040"]/div[1]').click();
-    await browser.pause(5000);
+    await browser.pause(1000);
+//click disable
     await $('//*[@id="checkbox-1054-inputEl"]').click();
-    await browser.pause(15000);
+    await browser.pause(1000);
+//Validacion disable
+    await $('//*[@id="button-1057"]').click();
+    await browser.pause(1000);
+//selecciona para habilitar 
+    await $('//*[@id="checkbox-1054-inputEl"]').click();
+    await browser.pause(1000);
+   
+// click volver buttons
+  await $('//*[@id="treeview-1030-record-11"]/tbody/tr/td').click();
+  await browser.pause(1000);
+ 
+  //################## Toogle Buttons #######################
+// Click Toggle Buttons
+await $('//*[@id="thumbnails-1040"]/div[2]').click();
+await browser.pause(1000);
+
+//click disable
+await $('/html/body/div[2]/div[2]/div[2]/div/div/div/div/div/div[1]/div/div/span/input').click();
+await browser.pause(1000);
+
+//selecciona para habilitar 
+await $('/html/body/div[2]/div[2]/div[2]/div/div/div/div/div/div[1]/div/div/span/input').click();
+await browser.pause(1000);
+ 
+// click volver buttons
+await $('//*[@id="treeview-1030-record-11"]/tbody/tr/td').click();
+await browser.pause(1000);
+
+  //################## Menu Buttons  #######################
+// Click Toggle Buttons
+await $('//*[@id="thumbnails-1040"]/div[3]').click();
+await browser.pause(1000);
+
+//click disable
+await $('/html/body/div[2]/div[2]/div[2]/div/div/div/div/div/div[1]/div/div/span/input').click();
+await browser.pause(1000);
+
+//selecciona para habilitar 
+await $('/html/body/div[2]/div[2]/div[2]/div/div/div/div/div/div[1]/div/div/span/input').click();
+await browser.pause(1000);
+
+//selecciona para lista 1
+await $('/html/body/div[2]/div[2]/div[2]/div/div/div/div/div/div[2]/table/tbody/tr[1]/td[2]/a').click();
+await browser.pause(1500);
+
+// click volver buttons
+await $('//*[@id="treeview-1030-record-11"]/tbody/tr/td').click();
+await browser.pause(1000);
+
+
+// click volver examples
+await $('/html/body/div[6]/div[2]/div/div/div[2]').click();
+await browser.pause(1000);
+
+// click volver home
+await $('/html/body/div[2]/div[2]/div[1]/div/div/a').click();
+await browser.pause(1500);
+
   }
-  /*public async clickOnComponent(){
-  }*/
+
 }
+
 
 export default new HomePage();
